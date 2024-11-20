fbaac2f021abcc2bad2c9e492aacecb5ae51712d
Khaled Khan <kkhan@khaledkhan.me>
2024-11-20 19:47:38 +1000

    further attempts at fixing navigation

--- src/Projects.js
+++ src/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
       <div className = {styles.box}>
         <div className = {styles.container}>
           <h2 className = {styles.header}>Portfolio Website</h2>
-          <p><Link to="">View</Link> | <a href="https://github.com/khaled-khan-47/Khaled-Khan-Portfolio-Website">Source</a></p>
+          <p><Link to="/">View</Link> | <a href="https://github.com/khaled-khan-47/Khaled-Khan-Portfolio-Website">Source</a></p>
           <p>Created this website from scratch, learning the basics of HTML, CSS, React.js and web hosting along the way.</p>
           <p>Nov 2024</p>
         </div>
